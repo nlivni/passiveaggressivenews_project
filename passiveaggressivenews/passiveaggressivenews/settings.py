e38bea687d87dbfd0c3f94693e31f0bad90e0274
nlivni <nathanlivni@gmail.com>
2014-09-27 13:19:42 -0500

    added postgres database to settings

--- passiveaggressivenews/passiveaggressivenews/settings.py
+++ passiveaggressivenews/passiveaggressivenews/settings.py
@@ -58,10 +58,13 @@ WSGI_APPLICATION = 'passiveaggressivenews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'panews',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
