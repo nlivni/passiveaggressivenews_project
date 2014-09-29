e662a441244ff9e80c16952da7634d810dc55e81
nlivni <nathanlivni@gmail.com>
2014-09-28 19:48:04 -0500

    added first models: story, category, etc.

--- passiveaggressivenews/panews/models.py
+++ passiveaggressivenews/panews/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from taggit.managers import TaggableManager
+import uuid
 
-# Create your models here.
+class Category(models.Model):
+    """
+    For the navigation bar. Each story has one and only one category.
+    """
+    name = models.CharField(max_length=30)
+
+
+class StoryArchtype(models.Model):
+    """
+    This is an abstract base class for our hardcoded story models. If we get around to having
+    """
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category)
+    tags = TaggableManager()
+    id = models.CharField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class StoryGnome(models.Model):
+    """
+    This is the actual story. The template will contain the text of the story, this model contains the variables
+    that will form the "story spine", also the fields of the form.
+    """
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    summary = models.TextField()
+
+
+class AssociatedSnippet(models.Model):
+    """
+    Appears at the side of the page. not clickable but relate to the general category
+    """
+    name = models.CharField(max_length=50)
+    text = models.TextField()
+# todo: create more generic story model
+
---
+++ passiveaggressivenews/passiveaggressivenews/views.py
@@ -0,0 +1,16 @@
+__author__ = 'nlivni'
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("<h1>hello world</h1>")
+
+#todo homepage
+
+
+
+#todo storylist
+
+
+#todo storylist by
