191ccd25dbcee2d354a3068e9dc53a19ad77a519
nlivni <nathanlivni@gmail.com>
2014-09-28 23:11:10 -0500

    registered panews  models in admin

--- passiveaggressivenews/panews/admin.py
+++ passiveaggressivenews/panews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from panews.models import Category, StoryArchtype, StoryGnome, AssociatedSnippet
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(StoryArchtype)
+admin.site.register(StoryGnome)
+admin.site.register(AssociatedSnippet)
