6872af53c732f8250a3ee2520cadbd9533a21405
nlivni <nathanlivni@gmail.com>
2014-10-01 13:35:05 -0500

    added panews templates, less files, no changes to bootstrap css - just testing less
    not sure re changes to models

--- passiveaggressivenews/panews/models.py
+++ passiveaggressivenews/panews/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from taggit.managers import TaggableManager
 import uuid
 
+def make_uuid():
+    return str(uuid.uuid1().int>>64)
 
 class Category(models.Model):
     """
@@ -20,8 +22,9 @@ class StoryArchtype(models.Model):
     name = models.CharField(max_length=50)
     category = models.ForeignKey(Category)
     tags = TaggableManager()
-    uuid = models.CharField(default=uuid.uuid4, editable=False, max_length=128)
-
+    #can't use out of the box w/class based models, which is the whole reason we're doing this.
+    #uuid = models.CharField(default=uuid.uuid4, editable=False, max_length=128)
+    id = models.CharField(max_length=36, primary_key=True, default=make_uuid)
     class Meta:
         abstract = True
 
@@ -37,6 +40,7 @@ class StoryGnome(StoryArchtype):
     title = models.CharField(max_length=50)
     subtitle = models.CharField(max_length=50)
     summary = models.TextField()
+
 
     def __str__(self):
         return self.title
