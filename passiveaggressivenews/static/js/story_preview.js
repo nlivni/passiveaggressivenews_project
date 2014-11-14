4f433f3e9492e67627169593fde77ec396a428fc
nlivni <nathanlivni@gmail.com>
2014-11-14 13:09:14 -0600

    story_preview.js appears. does not update variables

--- passiveaggressivenews/static/js/story_preview.js
+++ passiveaggressivenews/static/js/story_preview.js
@@ -2,13 +2,19 @@
  * Created by nlivni on 11/13/2014.
  */
 
+$(document).ready(function create_preview(){
+        var title = document.getElementById("id_title").value;
+        var template = document.getElementById("id_template").value;
+        var preview = document.getElementById("preview");
 
-$(document).keyup(
-function updateLoop(){
-var storytext = document.getElementById("story-text").value;
-var preview = document.getElementById("preview");
-preview.innerHTML = "<p>" + storytext + "</p>";
-preview.style.display = "block";
-}
-)
-  //  onKeyPress = updateLoop();
+        preview.innerHTML = "<h2>" + title + "</h2>" + "<p>" + template + "</p>";
+
+    }
+);
+
+$(document).keyup(function update_preview(){
+        var template = document.getElementById("id_template").value;
+        var preview = document.getElementById("preview");
+        preview.innerHTML = "<p>" + template + "</p>";
+    }
+);
